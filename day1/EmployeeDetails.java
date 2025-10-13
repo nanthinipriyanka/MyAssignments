446acaa40f472169ed6b57e8b334b9150a0b8cfa
nanthinipriyanka <nanthinipriyanka04@gmail.com>
2025-10-13 11:38:54 +0530

    Add files via upload
    Day 1 Activities

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) 
+	
+	{
+	
+ String name ="Nanthini Priyanka";
+ int age =34;
+ long salary=50000;
+ char gender='F';
+ long phonenumber =7397260151L;
+ String fathername ="Thangakkannan";
+ 
+System.out.println("Employee name:" +name + "\n" + "Employee Age is:" +age+"\n" +"Emp salary:" +salary+"\n" 
++"Emp Gender:" +gender+"\n"+"phonenumber:" +phonenumber+ "\n"+"Fathername is:" +fathername+ "\n");
+ 
+	}
+
+}
